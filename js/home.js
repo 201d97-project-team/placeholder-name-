01245a90393b1ad3965a6ab26b4d1779fdaff8f6
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-02-28 12:42:49 -1000

    save in prep for git event

--- js/home.js
+++ js/home.js
@@ -1,24 +1,22 @@
-//Global Variables
 
+// Moved to  content
+/* //Global Variables
 let cardArray = ['./red.jpeg','./green.jpeg','./blue.jpeg']; 
-
 let catArray = [];
-
 let dogArray = [];
+let birdArray = []; */ 
 
-let birdArray = [];
 
-//Form Control
+
+//Form Controls
 
 let submissionForm = document.getElementById('submission-form');
-
-submissionForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
   
   let name = event.target.name.value;
-  
+
   let color = event.target.color.value; //var from inside select or option element
   if (color === 'red') {
     color = cardArray[0];
@@ -40,32 +38,25 @@ function handleSubmit(event) {
   else {
     animal = birdArray;
   }
-
-  let newCard = new Cards(name, color, animalArray);
+    
+  localStorage(name,color,animal);
 }
 
-//Constructor Function
-
-function Cards(name, color, animalArray) {
-  this.name = name;
-  this.color = color;
-  this.animalArray = animalArray*2;   //we can verify card match through checking for equivalent strings at decisions. 
-  turnCounter = 0;
-  this.renderCards();
+function localStorage(name, color, animalArray) {
+  if (name != null || color != null || animalArray != null) {
+    let infoArray = [name,color,animalArray];
+    let stringifiedInfoArray = JSON.stringify(infoArray);
+    localStorage.setItem(stringifiedInfoArray);
+  }
+  else{
+    alert('Please fill out all fields');
+  }
 }
 
-//Prototype Function
+//Executable Code
 
-Cards.prototype.renderCards = function () {
-  let cards = document.getElementById('card-square');
-  cards.appendChild(this.color);
-}
-
-//Executable Code... technically this is happening inside the constructor function. save for pushing in pics of animals
+submissionForm.addEventListener('submit', handleSubmit);
 
 catArray.push();
 dogArray.push();
 birdArray.push();
-
-
-
