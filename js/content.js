393885bd2990ba2ae0dca0d2c60e4c473f26a528
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-03-01 08:39:13 -1000

    notes

--- js/content.js
+++ js/content.js
@@ -49,6 +49,7 @@ function createGame() {
   cardImages.forEach((cardImage, index) => {
     cardImage.addEventListener('click', () => {
       // Check if the card has already been selected
+      //add some sort of css property, if image clicked, apply. Make visibility hidden. 
       if (selectedCards.includes(index)) {
         return;
       }
@@ -83,39 +84,39 @@ function createGame() {
 }
 
 createGame();
-=======
+
 //Global Variables
 
-let cardArray = ['./red.jpeg','./green.jpeg','./blue.jpeg']; 
+// let cardArray = ['./red.jpeg','./green.jpeg','./blue.jpeg']; 
 
-let catArray = [];
+// let catArray = [];
 
-let dogArray = [];
+// let dogArray = [];
 
-let birdArray = [];
+// let birdArray = [];
 
-//Open Local Storage
-let formInfo = localStorage.getItem(infoArray);
+// //Open Local Storage
+// let formInfo = localStorage.getItem(infoArray);
 
-let name = formInfo[0];
-let color = formInfo[1];
-let animalArray = formInfo[2];
+// let name = formInfo[0];
+// let color = formInfo[1];
+// let animalArray = formInfo[2];
 
-//Constructor Function
+// //Constructor Function
 
-let newCard = new Cards(formInfo[0], formInfo[1], formInfo[2]);
+// let newCard = new Cards(formInfo[0], formInfo[1], formInfo[2]);
 
-function Cards(name, color, animalArray) {
-  this.name = name;
-  this.color = color;
-  this.animalArray = animalArray*2;   //we can verify card match through checking for equivalent strings at decisions. 
-  turnCounter = 0;
-  this.renderCards();
-}
+// function Cards(name, color, animalArray) {
+//   this.name = name;
+//   this.color = color;
+//   this.animalArray = animalArray*2;   //we can verify card match through checking for equivalent strings at decisions. 
+//   turnCounter = 0;
+//   this.renderCards();
+// }
 
-//Prototype Function
+// //Prototype Function
 
-Cards.prototype.renderCards = function () {
-  let cards = document.getElementById('card-square');
-  cards.appendChild(this.color);
-}
+// Cards.prototype.renderCards = function () {
+//   let cards = document.getElementById('card-square');
+//   cards.appendChild(this.color);
+// }
